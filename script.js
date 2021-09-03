333461200d66585f714abccf61c369321c7066e2
evansmaina <46711430+evansmaina@users.noreply.github.com>
2021-09-03 10:52:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
+
 jQuery(document).ready(function($) {
     "use strict"
 
-    // ------- Home Slider Start ------- //
+//navigation bar fixed top{
+	
+let nav_offset_top = $('header').height() + 50;
+
+    function navbarFixed() {
+        if ($('header').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('header nav').addClass('navbar_fixed');
+                 
+                    
+                } else {
+                    $('header nav').removeClass('navbar_fixed');
+                    
+                }
+            })
+        }
+    }
+
+    navbarFixed(); 
+	
+	
+	
+//    hero secyion slider
     if ($('#home-slider').length) {
         $('#home-slider').owlCarousel({
             loop: true,
@@ -13,8 +46,59 @@ jQuery(document).ready(function($) {
 
         })
     }
-    // ------- Home Slider End ------- //
-
+//   patner logos slider
+if ($('#partner-logos').length) {
+			$('#partner-logos').owlCarousel({
+				loop: true,
+				center: true,
+				autoplay: true,
+				margin: 30,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1,
+						loop: true,
+					},
+					600: {
+						items: 3,
+						loop: true,
+					},
+					1000: {
+						items: 5,
+						loop: true,
+	
+					}
+				}
+			})
+		}
+	
+	
+//	    Our customer reviews
+	        $('#customers-testimonials').owlCarousel({
+		            loop: true,
+		            center: true,
+		            items: 3,
+		            margin: 0,
+		            autoplay: true,
+		            dots:true,
+		            autoplayTimeout: 8500,
+		            smartSpeed: 450,
+		            responsive: {
+		              0: {
+		                items: 1
+		              },
+		              768: {
+		                items: 2
+		              },
+		              1170: {
+		                items: 3
+		              }
+		            }
+		        }); 
+	
+	
+//	scroll top button
+	
 
 	}); //End
 
